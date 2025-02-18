4fb0df8e2065e4158938f532dd34f4120dabd375
LEE-DHARAN <muralidhawan@gmail.com>
2025-02-18 16:33:55 +0530

    final

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  //baseURL: "https://be-hotelmanage.onrender.com/api/",
-  baseURL: "http://localhost:3001/api/",
+  baseURL: "https://be-hotelmanage.onrender.com/api/",
+  //baseURL: "http://localhost:3001/api/",
   headers: {
     "Content-Type": "application/json",
   },
